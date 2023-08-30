961e97906ee951840e338c02abf9d859ff6223a2
Juan Bautista <juanbrunet0001@gmail.com>
2023-08-30 14:36:44 -0300

    proyectojava

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let masa= parseInt(prompt("ingrese cantidad de altura"))
+let aceleracion=parseInt(prompt("ingrese cantidad de peso"))
+let resultado= masa*aceleracion
+
+
+if(resultado <= 10){
+ alert("no podes levantar nada") 
+}
+ else{
+  alert("sos muy fuerte")
+    
+ }
+
